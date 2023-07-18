cb39843cbbcc2889df2cbfb3c14d40e5c5d0a286
Fanny Nathaniella <87670527+FannyNathaniella001@users.noreply.github.com>
2023-07-17 21:55:56 -0400

    Update cnn.js

--- src/cnn/cnn.js
+++ src/cnn/cnn.js
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize(
+/*export const sequelize = new Sequelize(
   "postgres://postgres:admin@localhost:5432/curso"
+);*/
+export const sequelize = new Sequelize(
+  "postgres://postgres:t367cAYTyRrGkR3o7jap@containers-us-west-140.railway.app:7674/railway"
 );
